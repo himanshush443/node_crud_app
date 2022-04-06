ff2617a8e8920d5e3ce062b4311cd5f9051ef246
Himanshu Khare <himanshush443@gmail.com>
2022-04-06 18:21:51 +0530

    crud basics done

--- index.js
+++ index.js
@@ -15,20 +15,12 @@ app.get('/', (req,res) => {
 })
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
 })
 
 app.post('/api/courses', (req, res) => {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
-
-    const result = Joi.validate(req.body, schema);
-    
-    if(!req.body.name || req.body.name.length < 3) {
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
+    const { error } = validateCourse(req.body);
+    if(error)   return res.status(400).send(error.details[0].message);
 
     const course = {
         id: courses.length+1,
@@ -38,11 +30,40 @@ app.post('/api/courses', (req, res) => {
     res.send(course);
 })
 
-app.get('/api/courses/:id', (req, res) => {
+
+app.put('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course) res.status(404).send('There is no course with this id');
+    if(!course) return res.status(404).send('There is no course with this id');
+
+    const { error } = validateCourse(req.bdoy);
+    if(error)   return res.status(400).send(error.details[0].message);
+
+    course.name = req.body.name;
     res.send(course);
 })
+
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) return res.status(404).send('The course with this Id is not present');
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+    res.status(200).send(course);
+})
+
+
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) return res.status(404).send('There is no course with this id');
+    res.send(course);
+})
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(course, schema);
+}
 
 //Port
 const port = process.env.PORT || 3000;
